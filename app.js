c4ce4bdb73f0a80c90fe44dc3bb32d844db10b6c
Šarūnas Vaškelis <sarunas@tsolutions.io>
2019-05-03 16:31:32 +0300

    api now has hyperlinks

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (process.env.ENV === 'Test') {
   const db = mongoose.connect('mongodb://localhost/bookAPI_Test');
 } else {
   console.log('This is for real');
-  const db = mongoose.connect('mongodb://localhost/bookAPI-prod');
+  const db = mongoose.connect('mongodb://localhost/bookAPI');
 }
 
 const port = process.env.PORT || 3000;
